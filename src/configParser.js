1a34c539e3024082e2927cd5284002ddc3393d65
Simone Cornacchia <scornacchia@tierratelematics.com>
2017-08-03 18:15:30 +0200

    K***** API fix

--- src/configParser.js
+++ src/configParser.js
@@ -1,9 +1,10 @@
+var colors = require( "colors" );
 var _ = require( "lodash" );
 var fs = require( "fs" );
 var path = require( "path" );
 var when = require( "when" );
 
-var builtIn = getAdapters();
+//var builtIn = getAdapters();
 
 function defaultConstraint( config ) {
 	return function levelConstraint( data ) {
@@ -67,17 +68,61 @@ function wireUp( adapterFsm, config, channel, adapter ) {
 	adapter.subscriptions = subscriptions;
 }
 
+var adapter, lastConfig;
+function configure( config, formatter ) {
+	if( adapter && lastConfig && _.eq( lastConfig, config ) ) {
+		return;
+	}
+	lastConfig = config;
+	var envDebug = !!process.env.DEBUG;
+
+	var theme = _.extend( {
+		info: "green",
+		warn: "yellow",
+		debug: "blue",
+		error: "red"
+	}, config.theme );
+
+	var logType = {
+		info: "info",
+		warn: "warn",
+		debug: "log",
+		error: "error"
+	};
+
+	colors.setTheme( theme );
+
+	return {
+		onLog: function( data ) {
+			var msg;
+			if ( data.msg.toString() === "[object Object]" ) {
+				msg = config.formatJSON ? JSON.stringify( data.msg, null, 2 ) : JSON.stringify( data.msg );
+			} else {
+				msg = data.msg;
+			}
+			var timestamp = formatter( config, data );
+			console[logType[data.type]]( colors[ data.type ]( timestamp, "[" + data.namespace + "]" || "", msg ) );
+		},
+		constraint: function( data ) {
+			return data.level <= config.level && ( !config.bailIfDebug || ( config.bailIfDebug && !envDebug ) );
+		}
+	};
+}
+
+
 module.exports = function( channel, config, fount ) {
 	var adapterFsm = require( "./adapter.fsm" );
 
 	return _.map( config.adapters, function( adapterCfg, name ) {
+/*
 		var adapterPath;
 		if ( /[\/]/.test( name ) ) {
 			adapterPath = require.resolve( path.resolve( process.cwd(), name ) );
 		} else {
 			adapterPath = builtIn[ name ] || require.resolve( name );
 		}
-		var adapter = require( adapterPath )( adapterCfg, timeFormatter, fount );
+*/
+		var adapter = configure( adapterCfg, timeFormatter, fount );
 
 		wireUp( adapterFsm, adapterCfg, channel, adapter );
 
